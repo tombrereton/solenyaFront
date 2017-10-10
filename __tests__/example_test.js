53e7e9e26499ed0766d848514fdb35ca7d55afc1
Tom Brereton <Tom.Brereton@asos.com>
2017-10-10 09:46:10 +0100

    Add new react app using create-react-app command

--- __tests__/example_test.js
+++
@@ -1,5 +0,0 @@
-test('Sum of 1 + 1 should be 2', () => {
-	let actual = 1 + 2;
-	let expected = 2;
-	expect(expected).toBe(actual);
-});
--- server.babel.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.use('/', express.static('app'));
-
-app.listen(process.env.PORT || 3000);
