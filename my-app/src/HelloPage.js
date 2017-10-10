b8f6d84428851181a71022ff5e572fdac2992d1c
Catherine Sanft <Catherine.Sanft@asos.com>
2017-10-10 12:54:04 +0100

    added Hello page in react

---
+++ my-app/src/HelloPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './HelloPage.css';
+
+class HelloPage extends Component {
+  render() {
+    return (
+      <div className="HelloPage">
+          <h1 className="HelloPage-title">HELLO</h1>
+      </div>
+    );
+  }
+}
+
+export default HelloPage;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import HelloPage from './HelloPage';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HelloPage />, document.getElementById('root'));
 registerServiceWorker();
