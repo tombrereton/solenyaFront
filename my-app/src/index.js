82e9c67842acc2f0c862bdbd93e06cb2c2488c9a
Tom Brereton <Tom.Brereton@asos.com>
2017-10-11 09:52:22 +0100

    Separate productData from productPage

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import App from './App';
 import Root from './Root';
-
 
 ReactDOM.render(<Root />, document.getElementById('root'))
 
