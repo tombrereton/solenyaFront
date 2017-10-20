cd62e614045a473218f9242f23c629460ce0e730
Rasveer Bansi <rasveer.bansi@asos.com>
2017-10-20 16:56:59 +0100

    update tests to check fetch url and data

--- my-app/src/tests/dataFetcher.test.js
+++ my-app/src/tests/dataFetcher.test.js
@@ -3,12 +3,13 @@ import fetchMock from "fetch-mock";
 import { render } from "react-dom";
 import React from "react";
 import Root from '../Root';
-
+import testData from '../product_api_data_test.json';
 const productServiceEndpoint = 'http://team-solenya-product-dev.azurewebsites.net/';
 jest.mock("react-dom");
 
-const fetchData = { some: "result" };
-fetchMock.get("*", fetchData);
+
+const fetchData = testData[0];
+fetchMock.get(productServiceEndpoint, fetchData);
 
 afterEach(() => {
     jest.resetAllMocks();
@@ -16,10 +17,18 @@ afterEach(() => {
 describe("fetch and render", () => {
     it("calls fetch with the data URL", (done) => {
         fetchDataToJSON(productServiceEndpoint)
-            .then(() => {
-                expect(fetchMock.lastUrl()).toEqual('http://team-solenya-product-dev.azurewebsites.net/');
+            .then(data => {
+                expect(data).toEqual(fetchData);
                 done();
-            });
+            })
+    })
+
+    it("calls fetch with the right endpoint", (done) => {
+        fetchDataToJSON(productServiceEndpoint)
+        .then(() => {
+            expect(fetchMock.lastUrl()).toEqual(productServiceEndpoint);
+            done();
+        })
     })
 })
 
