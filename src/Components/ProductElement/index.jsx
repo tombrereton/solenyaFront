a17291298375e88b376d5795feb18f8361d51108
Tom Brereton <Tom.Brereton@asos.com>
2017-11-23 09:45:26 +0000

    Update Products and ProductElement css for basic overlapping look

--- src/Components/ProductElement/index.jsx
+++ src/Components/ProductElement/index.jsx
@@ -4,45 +4,48 @@ import { Link } from "react-router-dom";
 import Header from "../Header/index.jsx";
 import "./style.css";
 
-
 const ProductElement = props => {
-
-  
   return (
- 
     <div className="ProductElement" style={{ margin: "1em" }}>
-
-      <div className = "imageCard">
-        <Link 
-          id={"productImageId-" + props.ProductId}        
+      <div className="imageCard">
+        <Link
+          id={"productImageId-" + props.ProductId}
           className="productLink"
           to={"products/" + props.ProductId}
-          onClick={() => Header.onPdpLinkClicked(props.ProductName, props.ProductId, "-WithImage")}>
+          onClick={() =>
+            Header.onPdpLinkClicked(
+              props.ProductName,
+              props.ProductId,
+              "-WithImage"
+            )}
+        >
           <img width="150" src={props.SplashImgUrl} alt={props.ProductName} />
-        </Link>   
+        </Link>
       </div>
-   
-        <div className="productName" style={{ fontWeight: "bold" }}>
-          <div className = "infoCard">
-              <Link
-                id={"productNameId-" + props.ProductId}
-                className="productLink"
-                to={"products/" + props.ProductId}
-                onClick={() => Header.onPdpLinkClicked(props.ProductName, props.ProductId, "-WithName")}>
-              
-                {props.ProductName}
-              </Link>
-                  
-              <div className="productPrice">£{(props.Price / 100).toFixed(2)}</div>
-              <div className="productDiscount">
-                {props.DiscountPrice !== null && (
-                  <div id="discount">£{(props.DiscountPrice / 100).toFixed(2)}</div>
-                )}      
-              </div>          
-        </div>  
-      </div>  
-   </div>
-   
+
+      <div className="productName" >
+        <Link
+          id={"productNameId-" + props.ProductId}
+          className="productLink"
+          to={"products/" + props.ProductId}
+          onClick={() =>
+            Header.onPdpLinkClicked(
+              props.ProductName,
+              props.ProductId,
+              "-WithName"
+            )}
+        >
+          {props.ProductName}
+        </Link>
+
+        <div className="productPrice">£{(props.Price / 100).toFixed(2)}</div>
+        <div className="productDiscount">
+          {props.DiscountPrice !== null && (
+            <div id="discount">£{(props.DiscountPrice / 100).toFixed(2)}</div>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
