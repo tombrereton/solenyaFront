c768c11b275ce8a3e6ca449a1e1ff3121a47329e
Catherine Sanft <Catherine.Sanft@asos.com>
2017-12-07 16:26:41 +0000

    add a few e2e tests

--- nightwatch/test/e2e/e2eTest.js
+++ nightwatch/test/e2e/e2eTest.js
@@ -17,12 +17,77 @@ var config = {
         }
 }
 
-module.exports = { // adapted from: https://git.io/vodU0
-    'Solenya Couture End To End': function(browser) {
-      browser
+module.exports = {
+
+    beforeEach : function (browser) {
+        browser.resizeWindow(1280, 800);
+      }, 
+
+    'Solenya Couture renders': function(browser) {
+         browser
+            .url(config[envName].endPoint)
+            .assert.title("Solenya Couture")
+            .end()
+    },
+    
+    'Product image takes to the product details page and back to home with team name link': function(browser) {
+        browser
         .url(config[envName].endPoint)
         .waitForElementVisible('body')
-        .assert.title("Solenya Couture")
+        .pause(500)
+        .waitForElementVisible('.productLabelContainer')
+        .click('.productLabelContainer')
+        .waitForElementVisible('.webImageCarousel')
+        .assert.visible('.webImageCarousel')
+        .waitForElementVisible('.nameLink')
+        .pause(1000)
+        .click('.nameLink')
+        .pause(1000)
+        .end()
+    },
+
+    'Product image takes to the product details page and back to home with product link': function(browser) {
+        browser
+        .url(config[envName].endPoint)
+        .waitForElementVisible('body')
+        .pause(500)
+        .waitForElementVisible('.productLabelContainer')
+        .click('.productLabelContainer')
+        .waitForElementVisible('.webImageCarousel')
+        .assert.visible('.webImageCarousel')
+        .waitForElementVisible('.webHomeLink')
+        .pause(1000)
+        .click('.webHomeLink')
+        .pause(1000)
+        .end()
+    },
+    'Product image takes to the product details page and back to home with logo link': function(browser) {
+        browser
+        .url(config[envName].endPoint)
+        .waitForElementVisible('body')
+        .pause(500)
+        .waitForElementVisible('.productLabelContainer')
+        .click('.productLabelContainer')
+        .waitForElementVisible('.webImageCarousel')
+        .assert.visible('.webImageCarousel')
+        .waitForElementVisible('.logoLink')
+        .pause(1000)
+        .click('.logoLink')
+        .pause(1000)
+        .end()
+    },
+    'Product image takes to the product details page and clicks the carousel': function(browser) {
+        browser
+        .url(config[envName].endPoint)
+        .waitForElementVisible('body')
+        .pause(500)
+        .waitForElementVisible('.productLabelContainer')
+        .click('.productLabelContainer')
+        .waitForElementVisible('.webImageCarousel')
+        .assert.visible('.webImageCarousel')
+        .waitForElementVisible('.webImageCarousel > .slider .slider-decorator-1')
+        .click('.webImageCarousel > .slider > .slider-decorator-1')
+        .pause(1000)
         .end()
     }
   };
