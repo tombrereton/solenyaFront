21199eeeca99ac5751c6c58671312f34b6921a02
Rasveer Bansi <rasveer.bansi@asos.com>
2017-12-19 12:31:42 +0000

    fix lint errors

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Route, browserHistory} from "react-router-dom";
+import { BrowserRouter as Router, Route, browserHistory } from "react-router-dom";
 import Plp from "../Plp";
 import Pdp from "../Pdp";
 import "./style.css";
@@ -9,18 +9,18 @@ import Footer from "../Footer/index.jsx";
 const App = () => (
 
   <Router>
-  
+
     <div>
-      
+
       <Header />
-      
+
       <Route exact path="/" component={Plp} history={browserHistory} />
       <Route path="/products/:id" component={Pdp} />
 
       <Footer />
+
     </div>
 
-  
   </Router>
 
 );
